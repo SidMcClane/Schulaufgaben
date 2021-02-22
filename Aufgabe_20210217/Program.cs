43c44563ca5afdc6de28af648d1220973170660c
SidMcClane <sidmcclane@gmx.net>
2021-02-22 10:40:27 +0100

    Projektdateien hinzufügen.

---
+++ Aufgabe_20210217/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+/* ************************************************
+ * Programm: Aufgabe_20210217
+ * ************************************************
+ * Author: Gabriel Schultheisz
+ * Date: 22.02.2021
+ * 
+ * ************************************************
+ * Funktion: Formatierte Ausgabe der bekanntesten Datentypen und ihr Wertebereich
+ * ************************************************
+ * Weitere Quellen: 
+ * https://github.com/SidMcClane/ErsteApp
+ * 
+ */
+
+
+
+namespace Aufgabe_20210217 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("\n\n" +
+                "\t************************************************\n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n" +
+                "\t** \n");
+
+
+        }
+    }
+}
