bf788e63f105bdb0e97aad06090464e6029fd08d
SidMcClane <sidmcclane@gmx.net>
2021-03-09 14:24:10 +0100

    09.03.2021

---
+++ channel9Tutorials/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace channel9Tutorials {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10.");
+
+
+            Console.ReadKey();
+        }
+    }
+}
