8fb2ef5f31264a5a282580289eaf7322f0a1ccff
SidMcClane <sidmcclane@gmx.net>
2021-06-01 19:47:27 +0200

    01.06.2021 sync mit laptop

---
+++ Schulaufgabe_20210519/Functions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schulaufgabe_20210519 {
+    class Functions {
+
+        /// <summary>
+        /// Summiert alle Zahlen von der ersten Zahl bis zur letzten Zahl
+        /// </summary>
+        /// <param name="zahl_von">Die kleinere Zahl</param>
+        /// <param name="zahl_bis">Die größere Zahl</param>
+        /// <returns>int</returns>
+        public static int SummeFunc(int zahl_von, int zahl_bis) {
+            int ergeb = zahl_von;
+
+            while (zahl_von < zahl_bis) {
+                ergeb += zahl_bis;        // ergeb = ergeb + zahl_bis
+                zahl_bis--;
+            }
+            return ergeb;
+        }
+
+        /// <summary>
+        /// Multipliziert alle Zahlen von der ersten bis zur letzten Zahl
+        /// </summary>
+        /// <param name="zahl_von">Die kleinere Zahl</param>
+        /// <param name="zahl_bis">Die größere Zahl</param>
+        /// <returns>int</returns>
+        public static int Fakfunc(int zahl_von, int zahl_bis) {
+            int ergeb = zahl_von;
+
+            while (zahl_von < zahl_bis) {
+                ergeb *= zahl_bis;        // ergeb = ergeb * zahl_bis
+                zahl_bis--;
+            }
+            return ergeb;
+        }
+
+
+
+
+
+    }
+}
---
+++ Schulaufgabe_20210519/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+/* 
+ * ************************************************
+ * Programm: Schulaufgabe_20210519
+ * ************************************************
+ * Author: Gabriel Schultheisz
+ * Date: 19.05.2021
+ * 
+ * ************************************************
+ * Funktion: Ausgabe der Summe bzw Fakultät von Zahl 1 bis Zahl 2
+ * ************************************************
+ * Weitere Quellen: 
+ * https://github.com/SidMcClane/ErsteApp           - Bisheriger Aufgaben (Herr Faust)
+ * https://github.com/SidMcClane/Schulaufgaben      - Aufgaben ab 2021 (Herr Fecht)
+ * ************************************************
+ */
+
+namespace Schulaufgabe_20210519 {
+    class Program {
+        static void Main(string[] args) {
+
+            int zahl_1 = 2;
+            int zahl_2 = 8;
+            int summe = Functions.SummeFunc(zahl_1, zahl_2);
+            int fakultaet = Functions.Fakfunc(zahl_1, zahl_2);
+
+            Console.WriteLine($"Die Summe \tvon {zahl_1} bis {zahl_2} ist gleich: {summe}");
+            //Console.WriteLine(Convert.ToString(Functions.SummeFunc(zahl_1, zahl_2)));
+
+            Console.WriteLine($"Die Fakultät \tvon {zahl_1} bis {zahl_2} ist gleich: {fakultaet}");
+            //Console.WriteLine(Convert.ToString(Functions.Fakfunc(zahl_1, zahl_2)));
+        }
+    }
+}
