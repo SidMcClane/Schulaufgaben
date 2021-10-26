f9150ac830fc5d5f0e58f73f5c722b5c462de49b
SidMcClane <sidmcclane@gmx.net>
2021-10-26 21:19:42 +0200

    26.10.2021

---
+++ x-Tut_12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace x_Tut_Lists_01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine();
+            
+            var names = new List<string> { "Gabe", "Ana", "Felipe" };
+
+            names.Add("Gabba");
+            names.Add("Karina");
+            names.Add("Selly");
+            names.Add("Sara");
+            names.Add("Lena");
+
+            names.Remove("Gabe");
+            names.Remove("Ana");
+            names.Remove("Felipe");
+
+            names.Sort();
+            foreach (var name in names) {
+
+                Console.WriteLine($"Hallo {name}");
+            }
+
+            Console.WriteLine();
+            bool repeat = true;
+            while (repeat) {
+
+                Console.WriteLine("Gesuchten Namen eingeben");
+                string eingabe = Console.ReadLine();
+
+                Console.WriteLine();
+                var indey = names.IndexOf(eingabe);
+
+                if (indey == -1) {
+                    Console.WriteLine($"{eingabe} ist nicht in der Liste");
+                } else {
+                    Console.WriteLine($"{names[indey]} hat Index-Nr. {indey}");
+                }
+
+                Console.Write("Zum beenden der Abfrage 'no' eingeben: ");
+                if (Console.ReadLine() == "no") {
+                    repeat = false;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ x-Tut_Lists_02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x_Tut_Lists_02 {
+    class Program {
+        static void Main(string[] args) {
+
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add(previous + previous2);
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+
+            Console.WriteLine();
+
+            var fiboNumbers = new List<int> { 1, 1 };
+
+            for (int i = 3; i <= 20; i++) {
+
+            var ersteZahl = fiboNumbers[fiboNumbers.Count - 1];
+            var zweiteZahl = fiboNumbers[fiboNumbers.Count - 2];
+
+                fiboNumbers.Add(ersteZahl + zweiteZahl);
+            }
+
+            Console.WriteLine(fiboNumbers[fiboNumbers.Count -1]);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
