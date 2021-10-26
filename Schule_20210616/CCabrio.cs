0817f8ebef8cb974e7f3d38d598d732816ffb311
GSchultheisz <sidmcclane@gmx.net>
2021-10-26 21:20:17 +0200

    26.10.2021

---
+++ Schule_20210616/CCabrio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schule_20210616 {
+    public class CCabrio {
+        public int Property {
+            get => default;
+            set {
+            }
+        }
+    }
+}
---
+++ Schule_20210616/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+
+namespace Schule_20210616 {
+
+    // Zwei Klassen mit Unterklasse die erben und Objekt erstellen
+
+    class Program {
+
+        public struct structMeineStruktur {
+            public string Title;
+            public string Autor;
+            public int ISBN;
+            public double Preis;
+        }
+
+        
+
+
+        static void Main(string[] args) {
+
+            structMeineStruktur scParryHotter;
+
+            scParryHotter.Title = "Fluch blabla";
+            scParryHotter.Autor = "Depp";
+            scParryHotter.ISBN = 35797609;
+            scParryHotter.Preis = 34.99;
+
+
+            Console.WriteLine(scParryHotter.Title);
+
+
+
+            CFahrzeug OpelCorsa;
+            OpelCorsa = new CFahrzeug("Opel", "Corsa", 2006, "rot", 5, 86, 1500.44);
+            OpelCorsa.getAllData();
+
+            CFahrzeug OpelVectra = new CFahrzeug("Opel", "Vectra B", 1997, "blau", 5, 116, 600.00);
+            OpelVectra.getAllData();
+
+            
+            
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
